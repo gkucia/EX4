85a31b1ef45a5f53ea5630ceaaec94b8a387a298
gkucia <*KeySecret18*>
2015-04-28 15:37:41 +0400

    2

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication3
 {
+    public class Book {
+
+        //private ველების შექმნა
+        private string author;
+        private string book;
+        private int _pages;
+
+        //Print ფუნქცია
+        public void Print() { 
+            Console.WriteLine(_pages);
+        }
+
+        //_pages property
+        public int Pages{
+            get {
+                return this._pages;
+            }
+            set { //ვამოწმებთ გვერდების რაოდენობას
+                if (10 < value && value < 100)
+                {
+                    Console.WriteLine("Number of pages must be from 10 to 100");
+                }
+                else {
+                    this._pages = value;
+                }
+            }   
+        }
+
+
+
+
+    
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Book b1 = new Book();
+            int inp1 = Convert.ToInt32(Console.ReadLine());
+
+            b1.Print();
+
         }
     }
 }
